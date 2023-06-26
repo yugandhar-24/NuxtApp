45770dcf38f0ae07415fc731f362d4a54c93c58b
yugandhar <yugandhar_goud@trimble.com>
2023-06-26 16:38:17 +0530

    added nuxt js

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa soluta quo autem error eveniet numquam consequatur voluptatem vitae ipsam maxime.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa soluta quo autem error eveniet numquam consequatur voluptatem vitae ipsam maxime.</p>
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+h2{
+    margin-bottom:20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>    
+       <ProductDetails :product="product"/>
+    </div>
+</template>
+
+<script setup>
+
+const {id}=useRoute().params
+
+const url='https://fakestoreapi.com/products/'+id
+const {data:product}=await useFetch(url,{key:id})
+definePageMeta({
+        layout:'products'
+    })
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
